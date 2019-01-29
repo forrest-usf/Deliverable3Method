a809804cd65c8987e6615749698a3d38f1963c43
Mfbre <Mfbre@MATTHEW-DESKTOP>
2019-01-29 08:48:28 -0500

    version1

--- Deliverable3Method/Program.cs
+++ Deliverable3Method/Program.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+    Author: Matthew Bredahl
+    Date: 01/29/2019
+    Comments: This C# Console application code demonstrates the use of
+              a void method.
+*/
+
+using System;
 
 namespace Deliverable3Method
 {
@@ -6,7 +13,28 @@ namespace Deliverable3Method
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+            Console.Write("Please type in your name: ");
+
+            try
+            {
+                string input = Console.ReadLine();
+                Name(input);
+                Console.WriteLine("Press any key to exit the program . . . ");
+                Console.ReadKey(true);
+            }//End try
+
+            catch
+            {
+                Console.WriteLine("An error has occurred");
+                Console.WriteLine("Press any key to exit the program and try again . . . ");
+                Console.ReadKey(true);
+            }//End catch
+            
+        }//End Main method
+
+        private static void Name(string name)
+        {
+            Console.WriteLine("Hello " + name + "!");
+        }//End Name method
+    }//End Program class
+}//End Deliverable3Method namespace
